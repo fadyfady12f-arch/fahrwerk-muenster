20b3073a48184e54d90614facb00c82a2082ce81
Mosul <mosul@mac.tailc11ec4.ts.net>
2026-01-31 12:46:49 +0100

    Fix config for Vercel deployment

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',
   images: {
     unoptimized: true,
   },
-  basePath: '/fahrwerk-muenster',
-  assetPrefix: '/fahrwerk-muenster/',
 };
 
 export default nextConfig;
